e761c31117eadc13405c2966906876a4bf52afcd
Udbhav2006 <72188246+Udbhav2006@users.noreply.github.com>
2021-03-05 12:45:29 +0530

    Fixed some bugs...latest working as of 5/3/2021

--- README.md
+++
@@ -1,5 +0,0 @@
-This is the card game blackjack made with python. Enjoy!
-IMPORTANT: 
-You have to install the pillow package for it to run
-type the followig in the terminal to install pillow:
-pip install pillow
---
+++ fdsg.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import os
+
+# im = Image.open('Cards_pics/2 of clubs.png')
+# im.thumbnail((100,100))
+# im.save('Cards_pics/2 of clubs.png')
+# im.show()
+
+# obj = os.scandir('D:/Udbhav/Blackjack_rep/Cards_pics')
+# for pic in obj:
+#     im = Image.open(pic.path)
+#     im.show()
+a = Image.open('big_back_no_text.jpg')
+card = Image.open('Cards_pics/Ace of spades.png/')
+q = Image.open('Cards_pics/Queen of clubs.png')
+d = [card,q]
+im = a.copy()
+im.thumbnail((1300,650))
+i = 1
+for each in d:
+    im.paste(each, (i,1))
+    i+=82
+im.show()
+print(im.size)
+
